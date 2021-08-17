c2a763f4aa4709dc56000caa45b20eedebef3a79
seekuh <74740342+seekuh@users.noreply.github.com>
2021-08-17 15:49:50 +0200

    aufräumen

--- pggan/tfutil.py
+++ pggan/tfutil.py
@@ -1,10 +1,3 @@
-# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
-#
-# This work is licensed under the Creative Commons Attribution-NonCommercial
-# 4.0 International License. To view a copy of this license, visit
-# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
-# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
 import os
 import sys
 import inspect
@@ -13,10 +6,6 @@ import imp
 import numpy as np
 from collections import OrderedDict
 import tensorflow as tf
-
-
-# ----------------------------------------------------------------------------
-# Convenience.
 
 def run(*args, **kwargs):  # Run the specified ops in the default session.
     return tf.get_default_session().run(*args, **kwargs)
