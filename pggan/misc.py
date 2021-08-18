ac30a3af9e73e618df6e833cb1f5568cf247d45b
seekuh <anikaschmitt@gmx.net>
2021-08-18 15:40:05 +0000

    update

--- pggan/misc.py
+++ pggan/misc.py
@@ -1,11 +1,5 @@
-# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
-#
-# This work is licensed under the Creative Commons Attribution-NonCommercial
-# 4.0 International License. To view a copy of this license, visit
-# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
-# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
 import os
+import os.path
 import sys
 import glob
 import datetime
@@ -26,10 +20,30 @@ import shutil
 # Convenience wrappers for pickle that are able to load data produced by
 # older versions of the code.
 
-def load_pkl(filename):
+def load_pkl_single(filename):
     with open(filename, 'rb') as file:
         return legacy.LegacyUnpickler(file, encoding='latin1').load()
 
+def load_pkl(file_path):
+  n_bytes = 2**31
+  max_bytes = 2**31 - 1
+  data = bytearray(n_bytes)
+  ## write
+  bytes_out = pickle.dumps(data)
+  with open(file_path, 'wb') as f_out:
+      for idx in range(0, len(bytes_out), max_bytes):
+          f_out.write(bytes_out[idx:idx+max_bytes])
+
+  ## read
+  bytes_in = bytearray(0)
+  input_size = os.path.getsize(file_path)
+  with open(file_path, 'rb') as f_in:
+      for _ in range(0, input_size, max_bytes):
+          bytes_in += f_in.read(max_bytes)
+  data2 = pickle.loads(bytes_in)
+
+  assert(data == data2)
+  return data
 
 def save_pkl(obj, filename):
     with open(filename, 'wb') as file:
